97d8cea4f14fcc32e393e9e4c4ceb8ea7629e829
csAfter40 <csafter40@gmail.com>
2024-01-09 14:08:06 +0300

    feat: add main page

--- todoApp/App.js
+++ todoApp/App.js
@@ -1,20 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
 
 export default function App() {
+  const [items, setItems] = React.useState([])
+  const [itemText, setItemText] = React.useState("")
+  function handlePress() {
+    if (itemText != "") {
+      setItems(prevData => [...prevData, itemText])
+      setItemText("")
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <Text style={styles.h3}>Add items to your todo list.</Text>
+        <View style={styles.inputLine}>
+          <TextInput
+            style={styles.textInput}
+            value={itemText}
+            onChangeText={newText => setItemText(newText)}
+            placeholder='Enter text...'
+            placeholderTextColor='#eee'
+          />
+          <TouchableOpacity onPress={handlePress} style={styles.button}>
+            <Text style={styles.buttonText}>ADD ITEM</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.outputContainer}>
+        <Text style={styles.h3}>List of things to do:</Text>
+        <FlatList
+          data={items}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.listItem}>
+                <Text style={styles.listItemText}>* {itemData.item}</Text>
+              </View>
+            )
+          }}
+          keyExtractor={(itemData) => itemData.index}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    backgroundColor: "#966CFF",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    paddingTop: 70
   },
+  inputContainer: {
+
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+    alignItems: 'flex-start',
+    paddingBottom: 10,
+  },
+  inputLine: {
+    color: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: 'center',
+  },
+  outputContainer: {
+    paddingTop: 20,
+    flex: 8,
+  },
+  textInput: {
+    color: "#eee",
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    height: 40,
+    flex: 1,
+    marginRight: 6,
+    paddingLeft: 8
+  },
+  h3: {
+    color: "#eee",
+    fontSize: 20,
+    marginBottom: 10
+  },
+  listItem: {
+    backgroundColor: "#EFEEF6",
+    borderRadius: 6,
+    padding: 8,
+    marginVertical: 4,
+  },
+  listItemText: {
+    color: "#6339CC",
+  },
+  button: {
+    backgroundColor: "#14D3BA",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#eee",
+    paddingHorizontal: 12,
+    height: 40,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: "#eee",
+    fontSize: 15,
+  }
 });
