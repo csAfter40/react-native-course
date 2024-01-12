b83febb7cf9bcaa329347fc332a718f4be7fe628
csAfter40 <csafter40@gmail.com>
2024-01-12 09:47:55 +0300

    feat: add linear gradient for the background

--- guessTheNumber/App.js
+++ guessTheNumber/App.js
@@ -1,17 +1,20 @@
 import { StyleSheet, View } from "react-native";
 import PickNumberScreen from "./screens/PickNumberScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <View style={styles.rootContainer}>
+    <LinearGradient
+      colors={["#6b7c84", "#264653"]}
+      style={styles.rootContainer}
+    >
       <PickNumberScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    backgroundColor: "#264653",
   },
 });
