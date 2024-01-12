574589ba9d6bc5478024c7db2a1afcab98fc0c92
csAfter40 <csafter40@gmail.com>
2024-01-12 09:30:06 +0300

    feat: add ripple effect

--- guessTheNumber/components/Button.js
+++ guessTheNumber/components/Button.js
@@ -1,28 +1,33 @@
-import { Text, StyleSheet, TouchableHighlight } from "react-native";
+import { Text, StyleSheet, Pressable, View } from "react-native";
 import React from "react";
 
 export default function Button({ text, handlePress, color }) {
   return (
-    <TouchableHighlight
-      style={{ ...styles.button, backgroundColor: color }}
-      onPress={handlePress}
-      activeOpacity={1}
-      underlayColor="#F4A261"
-    >
-      <Text style={styles.text}>{text}</Text>
-    </TouchableHighlight>
+    <View style={styles.container}>
+      <Pressable
+        style={{ ...styles.button, backgroundColor: color }}
+        onPress={handlePress}
+        android_ripple={{ color: "#793a2b" }}
+      >
+        <Text style={styles.text}>{text}</Text>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 40,
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: "#E9C46A",
+    overflow: "hidden",
+  },
   button: {
     alignItems: "center",
     justifyContent: "center",
     padding: 8,
-    height: 40,
-    borderColor: "#E9C46A",
-    borderWidth: 1,
-    borderRadius: 100,
     flex: 1,
   },
   text: {
