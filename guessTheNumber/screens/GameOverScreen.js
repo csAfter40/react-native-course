85a53aa61f07915257888632ea1f1ce29552a843
csAfter40 <csafter40@gmail.com>
2024-01-14 18:21:58 +0300

    feat: add game over screen for cpu game

--- guessTheNumber/screens/GameOverScreen.js
+++ guessTheNumber/screens/GameOverScreen.js
@@ -1,30 +1,43 @@
-import { View, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
 import Button from "../components/Button";
+import UIContainer from "../components/UIContainer";
+import Page from "../components/Page";
+import Header from "../components/Header";
 
-export default function GameOverScreen({ setScreen }) {
+export default function GameOverScreen({
+  setScreen,
+  guessCount,
+  pickedNumber,
+  player,
+}) {
   function handleRestart() {
     setScreen("startGame");
   }
   return (
-    <View style={styles.page}>
-      <View style={styles.buttonContainer}>
-        <Button text={"Restart Game"} handlePress={handleRestart} />
-      </View>
-    </View>
+    <Page>
+      <UIContainer>
+        <Header text={`${player} found`} />
+        <Header text={pickedNumber} style={styles.bigText} />
+        <Header text={`in ${guessCount} guesses`} style={styles.smallText} />
+        <Button
+          text={"Restart Game"}
+          handlePress={handleRestart}
+          style={styles.button}
+        />
+      </UIContainer>
+    </Page>
   );
 }
 
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  button: {
+    width: "100%",
   },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
+  smallText: {
+    fontSize: 20,
+  },
+  bigText: {
+    fontSize: 40,
   },
 });
