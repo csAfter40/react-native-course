bc806baa60687621af259d4d96542c4339deb2c1
csAfter40 <csafter40@gmail.com>
2024-01-14 18:18:20 +0300

    feat: add GuessContainer component

---
+++ guessTheNumber/components/GuessContainer.js
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function GuessContainer({ text, style, textStyle }) {
+  return (
+    <View style={[styles.container, style]}>
+      <Text style={[styles.text, textStyle]}>{text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    backgroundColor: "white",
+    borderRadius: 1000,
+    alignItems: "flex-start",
+    paddingVertical: 8,
+    paddingHorizontal: 18,
+    justifyContent: "center",
+  },
+  text: {
+    color: "#E76F51",
+  },
+});
