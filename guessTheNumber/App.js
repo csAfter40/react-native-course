559ec98501774a9bab7e91ac986fb15c2bf383fb
csAfter40 <csafter40@gmail.com>
2024-01-18 23:25:58 +0300

    chore: set status bar style to light

--- guessTheNumber/App.js
+++ guessTheNumber/App.js
@@ -8,6 +8,7 @@ import StartGameScreen from "./screens/StartGameScreen";
 import CpuGameScreen from "./screens/CpuGameScreen";
 import UserGameScreen from "./screens/UserGameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
 	const [pickedNumber, setPickedNumber] = React.useState(null);
@@ -30,62 +31,65 @@ export default function App() {
 		setPickedNumber(number);
 	}
 	return (
-		<LinearGradient
-			colors={["#6b7c84", "#264653"]}
-			onLayout={onLayoutRootView}
-			style={styles.rootContainer}
-		>
-			<ImageBackground
-				source={require("./assets/images/bgImage.jpg")}
+		<>
+			<StatusBar style="light" />
+			<LinearGradient
+				colors={["#6b7c84", "#264653"]}
+				onLayout={onLayoutRootView}
 				style={styles.rootContainer}
-				imageStyle={styles.bgImage}
-				resizeMode="cover"
 			>
-				{screen === "startGame" ? (
-					<StartGameScreen
-						setScreen={setScreen}
-						setPickedNumber={setPickedNumber}
-						setGuessCount={setGuessCount}
-						setPlayer={setPlayer}
-					/>
-				) : screen === "pickNumber" ? (
-					<PickNumberScreen
-						handleNumberPick={handleNumberPick}
-						setScreen={setScreen}
-					/>
-				) : screen === "cpuGame" ? (
-					<CpuGameScreen
-						pickedNumber={pickedNumber}
-						guessCount={guessCount}
-						setGuessCount={setGuessCount}
-						setScreen={setScreen}
-					/>
-				) : screen === "userGame" ? (
-					<UserGameScreen
-						setPickedNumber={setPickedNumber}
-						pickedNumber={pickedNumber}
-						guessCount={guessCount}
-						setGuessCount={setGuessCount}
-						setScreen={setScreen}
-					/>
-				) : (
-					<GameOverScreen
-						setScreen={setScreen}
-						guessCount={guessCount}
-						pickedNumber={pickedNumber}
-						player={player}
-					/>
-				)}
-			</ImageBackground>
-		</LinearGradient>
+				<ImageBackground
+					source={require("./assets/images/bgImage.jpg")}
+					style={styles.rootContainer}
+					imageStyle={styles.bgImage}
+					resizeMode="cover"
+				>
+					{screen === "startGame" ? (
+						<StartGameScreen
+							setScreen={setScreen}
+							setPickedNumber={setPickedNumber}
+							setGuessCount={setGuessCount}
+							setPlayer={setPlayer}
+						/>
+					) : screen === "pickNumber" ? (
+						<PickNumberScreen
+							handleNumberPick={handleNumberPick}
+							setScreen={setScreen}
+						/>
+					) : screen === "cpuGame" ? (
+						<CpuGameScreen
+							pickedNumber={pickedNumber}
+							guessCount={guessCount}
+							setGuessCount={setGuessCount}
+							setScreen={setScreen}
+						/>
+					) : screen === "userGame" ? (
+						<UserGameScreen
+							setPickedNumber={setPickedNumber}
+							pickedNumber={pickedNumber}
+							guessCount={guessCount}
+							setGuessCount={setGuessCount}
+							setScreen={setScreen}
+						/>
+					) : (
+						<GameOverScreen
+							setScreen={setScreen}
+							guessCount={guessCount}
+							pickedNumber={pickedNumber}
+							player={player}
+						/>
+					)}
+				</ImageBackground>
+			</LinearGradient>
+		</>
 	);
 }
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-  },
-  bgImage: {
-    opacity: 0.1,
-  },
+	rootContainer: {
+		flex: 1,
+	},
+	bgImage: {
+		opacity: 0.1,
+	},
 });
