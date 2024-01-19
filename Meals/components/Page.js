131908db4dbbf5017090442e32b23ab380409d11
csAfter40 <csafter40@gmail.com>
2024-01-19 15:25:51 +0300

    feat: add Page component

---
+++ Meals/components/Page.js
@@ -0,0 +1,15 @@
+import { View, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Page({ children, style }) {
+	return <View style={[styles.page, style]}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+	page: {
+		flex: 1,
+		padding: 20,
+		paddingTop: 40,
+		width: "100%",
+	},
+});
