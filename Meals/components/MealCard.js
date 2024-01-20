4d702d9c63541a6dc7f87cc88aaa0f8227900485
csAfter40 <csafter40@gmail.com>
2024-01-20 23:17:02 +0300

    feat: set navigation from meal list to meal detail

--- Meals/components/MealCard.js
+++ Meals/components/MealCard.js
@@ -1,16 +1,17 @@
 import { View, Image, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 
-export default function MealCard({ meal }) {
-	console.log(meal.imageUrl);
+export default function MealCard({ meal, onMealSelect }) {
 	return (
-		<TouchableOpacity style={styles.container}>
+		<TouchableOpacity style={styles.container} onPress={onMealSelect}>
 			<Image style={styles.image} source={{ uri: meal.imageUrl }} />
 			<View style={styles.textContainer}>
-				<Text style={styles.text}>Title: {meal.title}</Text>
-				<Text style={styles.text}>Duration: {meal.duration} mins.</Text>
-				<Text style={styles.text}>Complexity: {meal.complexity}</Text>
-				<Text style={styles.text}>Affordability: {meal.affordability}</Text>
+				<Text style={[styles.text, styles.textHeader]}>{meal.title}</Text>
+				<View>
+					<Text style={styles.text}>Duration: {meal.duration} mins.</Text>
+					<Text style={styles.text}>Complexity: {meal.complexity}</Text>
+					<Text style={styles.text}>Affordability: {meal.affordability}</Text>
+				</View>
 			</View>
 		</TouchableOpacity>
 	);
@@ -29,19 +30,24 @@ const styles = StyleSheet.create({
 		overflow: "hidden",
 		padding: 10,
 		borderRadius: 8,
+		height: 120,
 	},
 	image: {
-		width: 140,
-		height: 140,
+		width: 100,
+		height: 100,
 		borderRadius: 4,
 	},
 	textContainer: {
 		flex: 1,
-		// backgroundColor: "red",
-		justifyContent: "flex-start",
+		justifyContent: "space-between",
 		height: "100%",
 	},
 	text: {
 		color: "black",
+	},
+	textHeader: {
+		fontWeight: "600",
+		marginBottom: 5,
+		fontSize: 16,
 	},
 });
--- Meals/screens/MealsList.js
+++ Meals/screens/MealsList.js
@@ -1,19 +1,29 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { StyleSheet, FlatList } from "react-native";
 import Page from "../components/Page";
 import React from "react";
 import { MEALS } from "../data/dummy-data";
 import { useRoute } from "@react-navigation/native";
 import MealCard from "../components/MealCard";
+import { useNavigation } from "@react-navigation/native";
 
 export default function MealsList() {
 	const route = useRoute();
 	const categoryId = route.params.categoryId;
 	const categoryMeals = MEALS.filter((meal) => meal.categoryIds.includes(categoryId));
+	const navigation = useNavigation();
+	function handleMealSelect(mealId) {
+		navigation.navigate("MealDetail", { mealId: mealId });
+	}
 	return (
 		<Page>
 			<FlatList
 				data={categoryMeals}
-				renderItem={({ item }) => <MealCard meal={item} />}
+				renderItem={({ item }) => (
+					<MealCard
+						meal={item}
+						onMealSelect={() => handleMealSelect(item.id)}
+					/>
+				)}
 				keyExtractor={(item) => item.id}
 			/>
 		</Page>
