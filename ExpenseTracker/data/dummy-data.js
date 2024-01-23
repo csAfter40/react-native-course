bf00af2d78aa3ef130cb61c0f150810bec297633
csAfter40 <csafter40@gmail.com>
2024-01-23 20:20:45 +0300

    feat: add models and data

---
+++ ExpenseTracker/data/dummy-data.js
@@ -0,0 +1,14 @@
+import Category from "../models/category";
+
+export const CATEGORIES = [
+	new Category("c1", "Housing", "#f5428d"),
+	new Category("c2", "Utilities", "#f54242"),
+	new Category("c3", "Transportation", "#f5a442"),
+	new Category("c4", "Food and Groceries", "#f5d142"),
+	new Category("c5", "Healthcare", "#368dff"),
+	new Category("c6", "Insurance", "#41d95d"),
+	new Category("c7", "Debt Payments", "#9eecff"),
+	new Category("c8", "Entertainment", "#b9ffb0"),
+	new Category("c9", "Personal Care", "#ffc7ff"),
+	new Category("c10", "Education", "#47fced"),
+];
---
+++ ExpenseTracker/models/expense.js
@@ -0,0 +1,11 @@
+class Expense {
+	constructor(id, amount, currency, date, category) {
+		this.id = id;
+		this.amount = amount;
+		this.currency = currency;
+		this.date = date;
+		this.category = category;
+	}
+}
+
+export default Expense;
