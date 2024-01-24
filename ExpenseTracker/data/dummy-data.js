d0d7765da4bc35b500d7ab4f5e77b3529aba81dc
csAfter40 <csafter40@gmail.com>
2024-01-24 08:03:21 +0300

    feat: add icon field to category model

--- ExpenseTracker/data/dummy-data.js
+++ ExpenseTracker/data/dummy-data.js
@@ -1,14 +1,14 @@
 import Category from "../models/category";
 
 export const CATEGORIES = [
-	new Category("c1", "Housing", "#f5428d"),
-	new Category("c2", "Utilities", "#f54242"),
-	new Category("c3", "Transportation", "#f5a442"),
-	new Category("c4", "Food and Groceries", "#f5d142"),
-	new Category("c5", "Healthcare", "#368dff"),
-	new Category("c6", "Insurance", "#41d95d"),
-	new Category("c7", "Debt Payments", "#9eecff"),
-	new Category("c8", "Entertainment", "#b9ffb0"),
-	new Category("c9", "Personal Care", "#ffc7ff"),
-	new Category("c10", "Education", "#47fced"),
+	new Category("c1", "Housing", "#f5428d", "home"),
+	new Category("c2", "Utilities", "#f54242", "lightning-bolt-circle"),
+	new Category("c3", "Transportation", "#f5a442", "train-car"),
+	new Category("c4", "Food and Groceries", "#f5d142", "cart-variant"),
+	new Category("c5", "Healthcare", "#368dff", "medical-bag"),
+	new Category("c6", "Insurance", "#41d95d", "shield-account"),
+	new Category("c7", "Debt Payments", "#9eecff", "cash-clock"),
+	new Category("c8", "Entertainment", "#b9ffb0", "drama-masks"),
+	new Category("c9", "Personal Care", "#ffc7ff", "face-woman-shimmer"),
+	new Category("c10", "Education", "#47fced", "school"),
 ];
