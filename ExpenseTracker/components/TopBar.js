add84c0aa5707e9a2aa18b2e81ca1ef9141eb313
csAfter40 <csafter40@gmail.com>
2024-01-25 06:17:26 +0300

    feat: edit TobBar to handle multiple navigation modes

--- ExpenseTracker/components/TopBar.js
+++ ExpenseTracker/components/TopBar.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { Appbar } from "react-native-paper";
+import { getHeaderTitle } from "@react-navigation/elements";
 
-export default function TopBar({ title, handlePressPlus }) {
+export default function TopBar({
+	title,
+	handlePressPlus,
+	navigation,
+	route,
+	options,
+	back,
+}) {
+	const barTitle = title || getHeaderTitle(options, route.name);
 	return (
 		<Appbar.Header elevated={true} mode="center-aligned">
-			<Appbar.Content title={title} />
+			{back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+			<Appbar.Content title={barTitle} />
 			<Appbar.Action icon="plus" onPress={handlePressPlus} />
 		</Appbar.Header>
 	);
