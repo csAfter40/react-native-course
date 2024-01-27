e32a147a00cf0ebe114337a107de66459b050edb
csAfter40 <csafter40@gmail.com>
2024-01-27 22:50:53 +0300

    feat: add modal for plus button

--- ExpenseTracker/components/TopBar.js
+++ ExpenseTracker/components/TopBar.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Appbar } from "react-native-paper";
 import { getHeaderTitle } from "@react-navigation/elements";
+import AddExpenseModal from "./AddExpenseModal";
 
-export default function TopBar({
-	title,
-	handlePressPlus,
-	navigation,
-	route,
-	options,
-	back,
-}) {
+export default function TopBar({ title, navigation, route, options, back }) {
 	const barTitle = title || getHeaderTitle(options, route.name);
+	const [visible, setVisible] = React.useState(false);
+	const showModal = () => setVisible(true);
+	const hideModal = () => setVisible(false);
 	return (
-		<Appbar.Header elevated={true} mode="center-aligned">
-			{back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-			<Appbar.Content title={barTitle} />
-			<Appbar.Action icon="plus" onPress={handlePressPlus} />
-		</Appbar.Header>
+		<>
+			<Appbar.Header elevated={true} mode="center-aligned">
+				{back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+				<Appbar.Content title={barTitle} />
+				<Appbar.Action icon="plus" onPress={showModal} />
+			</Appbar.Header>
+			<AddExpenseModal visible={visible} hideModal={hideModal} />
+		</>
 	);
 }
