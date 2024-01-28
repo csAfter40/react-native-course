0410e9a8a9353c068cb6142b6b21450574f315d2
csAfter40 <csafter40@gmail.com>
2024-01-28 09:18:02 +0300

    chore: register locales for react-native-paper-dates

--- ExpenseTracker/App.js
+++ ExpenseTracker/App.js
@@ -3,6 +3,11 @@ import { PaperProvider } from "react-native-paper";
 import { DataProvider } from "./context/DataProvider";
 import BottomTabNavigation from "./components/navigation/BottomTabNavigation";
 import { NavigationContainer } from "@react-navigation/native";
+import { enGB, tr, en, fr, registerTranslation } from "react-native-paper-dates";
+registerTranslation("en-GB", enGB);
+registerTranslation("en", en);
+registerTranslation("tr", tr);
+registerTranslation("fr", fr);
 
 export default function App() {
 	return (
