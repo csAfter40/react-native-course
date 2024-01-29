87bcd5900eec565f836a8208d475b31ddc39536f
csAfter40 <csafter40@gmail.com>
2024-01-29 23:03:51 +0300

    feat: edit expense modal opens when component pressed

--- ExpenseTracker/components/ExpenseCard.js
+++ ExpenseTracker/components/ExpenseCard.js
@@ -3,21 +3,38 @@ import React from "react";
 import { Surface, Avatar, Card, Text } from "react-native-paper";
 import { Pressable } from "react-native";
 import { getCategory } from "../utils";
+import ManageExpenseModal from "./ManageExpenseModal";
 
 export default function ExpenseCard({ expense, onExpenseSelect }) {
 	const category = getCategory(expense.category);
+	const [visible, setVisible] = React.useState(false);
+	function showModal() {
+		setVisible(true);
+	}
+	function hideModal() {
+		setVisible(false);
+	}
 	return (
-		<Pressable onPress={onExpenseSelect}>
-			<Surface style={styles.container}>
-				<Card.Title
-					title={expense.title}
-					titleStyle={styles.title}
-					subtitle={`${expense.amount} ${expense.currency}`}
-					left={(props) => <Avatar.Icon {...props} icon={category.icon} />}
-					right={(props) => <Text>{expense.date.toLocaleDateString()}</Text>}
-				/>
-			</Surface>
-		</Pressable>
+		<>
+			<Pressable onPress={showModal}>
+				<Surface style={styles.container}>
+					<Card.Title
+						title={expense.title}
+						titleStyle={styles.title}
+						subtitle={`${expense.amount} ${expense.currency}`}
+						left={(props) => <Avatar.Icon {...props} icon={category.icon} />}
+						right={(props) => (
+							<Text>{expense.date.toLocaleDateString()}</Text>
+						)}
+					/>
+				</Surface>
+			</Pressable>
+			<ManageExpenseModal
+				expense={expense}
+				visible={visible}
+				hideModal={hideModal}
+			/>
+		</>
 	);
 }
 
