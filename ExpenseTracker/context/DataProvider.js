7ae05a7357f793f5d9d6c996b6b709cb3fb378c2
csAfter40 <csafter40@gmail.com>
2024-02-01 06:43:15 +0300

    feat: add refresh expenses function to be called after crud operations

--- ExpenseTracker/context/DataProvider.js
+++ ExpenseTracker/context/DataProvider.js
@@ -1,17 +1,31 @@
 import React, { createContext } from "react";
-import { TEST_EXPENSES } from "../data/dummy-data";
+import axiosInstance from "../axios";
 
 const DataContext = createContext();
 
 function DataProvider(props) {
-	const [expenses, setExpenses] = React.useState(TEST_EXPENSES);
+	const [expenses, setExpenses] = React.useState([]);
+	function refreshExpenses() {
+		let newExpenses = [];
+		axiosInstance.get("/expenses.json").then((response) => {
+			for (const [key, value] of Object.entries(response.data)) {
+				value.date = new Date(value.date);
+				newExpenses.push({ ...value, id: key });
+			}
+			setExpenses(newExpenses);
+		});
+	}
+	React.useEffect(() => {
+		refreshExpenses();
+	}, []);
 	function removeFromExpenses(expenseId) {
 		setExpenses((prevList) => {
 			return prevList.filter((expense) => expense.id != expenseId);
 		});
 	}
-	function addToExpenses(expense) {
-		setExpenses((prevList) => [...prevList, expense]);
+	function addToExpenses(expenseId, data) {
+		const newExpense = { ...data, id: expenseId };
+		setExpenses((prevList) => [...prevList, newExpense]);
 	}
 	function editExpense(expenseId, data) {
 		setExpenses((prevList) => {
@@ -31,6 +45,7 @@ function DataProvider(props) {
 				removeFromExpenses,
 				addToExpenses,
 				editExpense,
+				refreshExpenses,
 			}}
 		>
 			{props.children}
