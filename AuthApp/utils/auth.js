038537ba5e1d42e527eda8fed8f21d0825c468d0
csAfter40 <csafter40@gmail.com>
2024-02-02 20:47:25 +0300

    feat: add auth util functions

---
+++ AuthApp/utils/auth.js
@@ -0,0 +1,17 @@
+import axiosInstance from "../axios";
+import { FIREBASE_API_KEY } from "@env";
+
+export async function authenticate(mode, email, password) {
+	let path;
+	if (mode === "signin") {
+		path = `/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`;
+	} else if (mode == "signup") {
+		path = `/accounts:signUp?key=${FIREBASE_API_KEY}`;
+	}
+	const response = await axiosInstance.post(path, {
+		email: email,
+		password: password,
+		returnSecureToken: true,
+	});
+	return response;
+}
