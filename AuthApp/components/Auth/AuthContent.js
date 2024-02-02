161b3f0e5ba6b74b68bd6f1661edcbf439f69571
csAfter40 <csafter40@gmail.com>
2024-02-02 20:48:52 +0300

    feat: set navigation between logina nd sign in screens

--- AuthApp/components/Auth/AuthContent.js
+++ AuthApp/components/Auth/AuthContent.js
@@ -4,62 +4,68 @@ import { Alert, StyleSheet, View } from 'react-native';
 import FlatButton from '../ui/FlatButton';
 import AuthForm from './AuthForm';
 import { Colors } from '../../constants/styles';
+import { useNavigation } from "@react-navigation/native";
 
 function AuthContent({ isLogin, onAuthenticate }) {
+	const navigation = useNavigation();
+	const [credentialsInvalid, setCredentialsInvalid] = useState({
+		email: false,
+		password: false,
+		confirmEmail: false,
+		confirmPassword: false,
+	});
 
-  const [credentialsInvalid, setCredentialsInvalid] = useState({
-    email: false,
-    password: false,
-    confirmEmail: false,
-    confirmPassword: false,
-  });
+	function switchAuthModeHandler() {
+		if (isLogin) {
+			// replace function also navigates to the screen but not gives a back button as navigate function
+			navigation.replace("Signup");
+		} else {
+			navigation.replace("Login");
+		}
+	}
 
-  function switchAuthModeHandler() {
-    // Todo
-  }
+	function submitHandler(credentials) {
+		let { email, confirmEmail, password, confirmPassword } = credentials;
 
-  function submitHandler(credentials) {
-    let { email, confirmEmail, password, confirmPassword } = credentials;
+		email = email.trim();
+		password = password.trim();
 
-    email = email.trim();
-    password = password.trim();
+		const emailIsValid = email.includes("@");
+		const passwordIsValid = password.length > 6;
+		const emailsAreEqual = email === confirmEmail;
+		const passwordsAreEqual = password === confirmPassword;
 
-    const emailIsValid = email.includes('@');
-    const passwordIsValid = password.length > 6;
-    const emailsAreEqual = email === confirmEmail;
-    const passwordsAreEqual = password === confirmPassword;
+		if (
+			!emailIsValid ||
+			!passwordIsValid ||
+			(!isLogin && (!emailsAreEqual || !passwordsAreEqual))
+		) {
+			Alert.alert("Invalid input", "Please check your entered credentials.");
+			setCredentialsInvalid({
+				email: !emailIsValid,
+				confirmEmail: !emailIsValid || !emailsAreEqual,
+				password: !passwordIsValid,
+				confirmPassword: !passwordIsValid || !passwordsAreEqual,
+			});
+			return;
+		}
+		onAuthenticate(email, password);
+	}
 
-    if (
-      !emailIsValid ||
-      !passwordIsValid ||
-      (!isLogin && (!emailsAreEqual || !passwordsAreEqual))
-    ) {
-      Alert.alert('Invalid input', 'Please check your entered credentials.');
-      setCredentialsInvalid({
-        email: !emailIsValid,
-        confirmEmail: !emailIsValid || !emailsAreEqual,
-        password: !passwordIsValid,
-        confirmPassword: !passwordIsValid || !passwordsAreEqual,
-      });
-      return;
-    }
-    onAuthenticate({ email, password });
-  }
-
-  return (
-    <View style={styles.authContent}>
-      <AuthForm
-        isLogin={isLogin}
-        onSubmit={submitHandler}
-        credentialsInvalid={credentialsInvalid}
-      />
-      <View style={styles.buttons}>
-        <FlatButton onPress={switchAuthModeHandler}>
-          {isLogin ? 'Create a new user' : 'Log in instead'}
-        </FlatButton>
-      </View>
-    </View>
-  );
+	return (
+		<View style={styles.authContent}>
+			<AuthForm
+				isLogin={isLogin}
+				onSubmit={submitHandler}
+				credentialsInvalid={credentialsInvalid}
+			/>
+			<View style={styles.buttons}>
+				<FlatButton onPress={switchAuthModeHandler}>
+					{isLogin ? "Create a new user" : "Log in instead"}
+				</FlatButton>
+			</View>
+		</View>
+	);
 }
 
 export default AuthContent;
