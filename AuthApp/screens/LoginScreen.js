0a8e86463d85347aa6cb730174f8488a8890179c
csAfter40 <csafter40@gmail.com>
2024-02-02 20:49:27 +0300

    feat: handle form submissions

--- AuthApp/screens/LoginScreen.js
+++ AuthApp/screens/LoginScreen.js
@@ -1,7 +1,27 @@
 import AuthContent from '../components/Auth/AuthContent';
+import React from "react";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { authenticate } from "../utils/auth";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+	const [isAuthenticating, setIsAuthenticating] = React.useState(false);
+
+	async function signinHandler(email, password) {
+		setIsAuthenticating(true);
+		try {
+			const response = await authenticate("signin", email, password);
+			console.log(response);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsAuthenticating(false);
+		}
+	}
+	return isAuthenticating ? (
+		<LoadingOverlay message="Logging in..." />
+	) : (
+		<AuthContent isLogin onAuthenticate={signinHandler} />
+	);
 }
 
 export default LoginScreen;
--- AuthApp/screens/SignupScreen.js
+++ AuthApp/screens/SignupScreen.js
@@ -1,7 +1,25 @@
 import AuthContent from '../components/Auth/AuthContent';
+import { authenticate } from "../utils/auth";
+import React from "react";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
-  return <AuthContent />;
+	const [isAuthenticating, setIsAuthenticating] = React.useState(false);
+	async function signupHandler(email, password) {
+		setIsAuthenticating(true);
+		try {
+			const response = await authenticate("signup", email, password);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsAuthenticating(false);
+		}
+	}
+	return isAuthenticating ? (
+		<LoadingOverlay message="Creating user..." />
+	) : (
+		<AuthContent onAuthenticate={signupHandler} />
+	);
 }
 
 export default SignupScreen;
