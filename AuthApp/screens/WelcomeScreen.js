98423f7a19818eb56d1f420480b57973e8dcc255
csAfter40 <csafter40@gmail.com>
2024-02-03 07:37:18 +0300

    feat: welcone screen now fetches  and ş-displays message from db

--- AuthApp/screens/WelcomeScreen.js
+++ AuthApp/screens/WelcomeScreen.js
@@ -1,12 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { axiosDbInstance } from "../axios";
+import { Alert } from "react-native";
+import { AuthContext } from "../contexts/AuthProvider";
 
 function WelcomeScreen() {
-  return (
-    <View style={styles.rootContainer}>
-      <Text style={styles.title}>Welcome!</Text>
-      <Text>You authenticated successfully!</Text>
-    </View>
-  );
+  const { token } = React.useContext(AuthContext);
+  const [message, setMessage] = React.useState("");
+  React.useEffect(() => {
+		token &&
+			axiosDbInstance
+				.get(`/message.json?auth=${token}`)
+				.then((response) => setMessage(response.data))
+				.catch((error) => Alert.alert("Unable to fetch message"));
+  }, [token]);
+	return (
+		<View style={styles.rootContainer}>
+			<Text style={styles.title}>Welcome!</Text>
+			<Text>You authenticated successfully!</Text>
+			<Text style={{ marginTop: 20, fontWeight: "600", fontSize: 16 }}>
+				Your message:
+			</Text>
+			<Text>{message}</Text>
+		</View>
+	);
 }
 
 export default WelcomeScreen;
