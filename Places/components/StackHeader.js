cebb5d3d75644b3b5b6fb7be242ce0ee000b6f9a
csAfter40 <csafter40@gmail.com>
2024-02-04 13:56:11 +0300

    ref: remove redundant code

--- Places/components/StackHeader.js
+++ Places/components/StackHeader.js
@@ -1,16 +1,14 @@
 import { Appbar } from "react-native-paper";
 import React from "react";
-import { useNavigation } from "@react-navigation/native";
 
 export default function StackHeader({ navigation, route, options, back }) {
-	const stackNavigation = useNavigation();
 	const screenButtons = {
 		AddPlace: [],
 		AllPlaces: ["plus"],
 		PlaceDetail: [],
 	};
 	function onPlusPress() {
-		stackNavigation.navigate("AddPlace");
+		navigation.navigate("AddPlace");
 	}
 	return (
 		<Appbar.Header mode="center-aligned" elevated={true}>
