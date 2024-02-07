acc3ea413f2f687378c6a2002af598e5eaf20113
csAfter40 <csafter40@gmail.com>
2024-02-07 13:42:30 +0300

    feat: add AddPlace screen

--- Places/screens/AddPlace.js
+++ Places/screens/AddPlace.js
@@ -1,11 +1,28 @@
-import { View, Text } from "react-native";
+import { Text } from "react-native-paper";
+import { StyleSheet, ScrollView } from "react-native";
 import Page from "../components/Page";
 import React from "react";
+import PlaceForm from "../components/PlaceForm";
 
 export default function AddPlace() {
 	return (
 		<Page>
-			<Text>This is Add Place screen</Text>
+			<ScrollView>
+				<Text style={styles.title}>Add New Favourite Place</Text>
+				<PlaceForm />
+			</ScrollView>
 		</Page>
 	);
 }
+
+const styles = StyleSheet.create({
+	page: {
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 20,
+		marginBottom: 5,
+		alignSelf: "center",
+	},
+});
