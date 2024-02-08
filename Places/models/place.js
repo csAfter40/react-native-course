567ea775283dbe36c26afa2fde48c1a5141c35e8
csAfter40 <csafter40@gmail.com>
2024-02-08 21:19:58 +0300

    ref: edit Place model to match with db table

--- Places/models/place.js
+++ Places/models/place.js
@@ -1,11 +1,10 @@
 class Place {
-	static nextId = 1;
-	constructor(title, imageUri, address, location) {
-		this.id = Place.nextId++;
+	constructor(title, imageUri, address, latitude, longitude) {
 		this.title = title;
 		this.imageUri = imageUri;
 		this.address = address;
-		this.location = location; // {lat: 12.234, lng: 34.23423}
+		this.latitude = latitude;
+		this.longitude = longitude;
 	}
 }
 
