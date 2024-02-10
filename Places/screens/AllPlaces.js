0313813ce3d076a6ed6b6e68bcbfd8d38ece7e2e
csAfter40 <csafter40@gmail.com>
2024-02-10 08:07:25 +0300

    feat: add all places screen

--- Places/screens/AllPlaces.js
+++ Places/screens/AllPlaces.js
@@ -1,11 +1,22 @@
-import { View, Text } from "react-native";
 import Page from "../components/Page";
 import React from "react";
+import PlacesList from "../components/PlacesList";
+import { getAllPlaces } from "../utils/database";
+import { Alert } from "react-native";
 
 export default function AllPlaces() {
+	const [places, setPlaces] = React.useState([]);
+	React.useEffect(() => {
+		getAllPlaces()
+			.then((res) => setPlaces(res))
+			.catch((err) => {
+				Alert.alert("Error getting data from database");
+				console.log(err);
+			});
+	}, []);
 	return (
 		<Page>
-			<Text>This is AllPlaces screen</Text>
+			<PlacesList places={places} />
 		</Page>
 	);
 }
