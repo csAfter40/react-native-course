b16a41aca8f73fc3ac4d0b75aaffdf940ef4bf91
csAfter40 <csafter40@gmail.com>
2024-02-12 13:38:09 +0300

    feat: add spinner

--- Places/screens/AllPlaces.js
+++ Places/screens/AllPlaces.js
@@ -3,16 +3,20 @@ import React from "react";
 import PlacesList from "../components/PlacesList";
 import { getAllPlaces } from "../utils/database";
 import { Alert } from "react-native";
+import { SpinnerContext } from "../context/SpinnerProvider";
 
 export default function AllPlaces() {
+	const { startSpinner, stopSpinner } = React.useContext(SpinnerContext);
 	const [places, setPlaces] = React.useState([]);
 	React.useEffect(() => {
+		startSpinner();
 		getAllPlaces()
 			.then((res) => setPlaces(res))
 			.catch((err) => {
 				Alert.alert("Error getting data from database");
 				console.log(err);
-			});
+			})
+			.finally(stopSpinner);
 	}, []);
 	return (
 		<Page>
