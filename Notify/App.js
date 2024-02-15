bed9a25aa9b470668b2a1ad2944883e21c618fed
csAfter40 <csafter40@gmail.com>
2024-02-15 23:43:21 +0300

    feat: setup notification plugin and add notification handler

--- Notify/App.js
+++ Notify/App.js
@@ -1,13 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as Notificatons from "expo-notifications";
+
+Notificatons.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: false,
+		shouldSetBadge: false,
+	}),
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	function scheduleNotificationHandler() {
+		Notificatons.scheduleNotificationAsync({
+			content: {
+				title: "Here comes a notification",
+				body: "This is great!",
+				data: { userName: "Max" },
+			},
+			trigger: {
+				seconds: 5,
+			},
+		});
+	}
+	return (
+		<View style={styles.container}>
+			<Button onPress={scheduleNotificationHandler} title="Schedule Notification" />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
