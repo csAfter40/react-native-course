9c95e8826b13b82d8ad8a1c624897b61f03ac767
csAfter40 <csafter40@gmail.com>
2024-02-16 08:46:59 +0300

    feat: add listener for user response to notification

--- Notify/App.js
+++ Notify/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from "react-native";
 import * as Notificatons from "expo-notifications";
+import React from "react";
 
 Notificatons.setNotificationHandler({
 	handleNotification: async () => ({
@@ -23,6 +24,24 @@ export default function App() {
 			},
 		});
 	}
+	React.useEffect(() => {
+		const subscription1 = Notificatons.addNotificationReceivedListener(
+			(notification) => {
+				console.log("notification received");
+				console.log(notification.request.content.data);
+			}
+		);
+		const subscription2 = Notificatons.addNotificationResponseReceivedListener(
+			(response) => {
+				console.log("notification response received");
+				console.log(response);
+			}
+		);
+		return () => {
+			subscription1.remove();
+			subscription2.remove();
+		};
+	}, []);
 	return (
 		<View style={styles.container}>
 			<Button onPress={scheduleNotificationHandler} title="Schedule Notification" />
